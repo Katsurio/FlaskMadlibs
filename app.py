3b59ccfe9e0bbb3ad06eab6d54a173f615949f4c
John E. Fisher (Jef) <fishe545@gmail.com>
2021-08-26 20:25:46 +0800

    feat: getting form data and displaying story on page

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
-from random import choice, sample, randint
-
+from flask import Flask, render_template, redirect
+from flask.globals import request
 from flask_debugtoolbar import DebugToolbarExtension
 
+from Stories import Story
+from MadLibsForm import MadLibForm
 
 COMPLIMENTS = ["cool", "clever", "tenacious", "awesome", "Pythonic"]
 
@@ -13,7 +14,46 @@ debug = DebugToolbarExtension(app)
 
 
 @app.route('/')
-def index():
+def home():
     """Return homepage."""
+    form = MadLibForm()
+    msg = ""
+    if form.validate_on_submit():
+        place = form.place.data
+        noun = form.noun.data
+        verb = form.verb.data
+        adjective = form.adjective.data
+        plural_noun = form.plural_noun.data
+        
+        return redirect('/story')
+    else: 
+        msg = "Error"
+    return render_template("home.html", form = form)
 
-    return render_template("home.html")
+
+
+
+@app.route('/story', methods=['POST'])
+def post_story():
+    """Return madlib story from form submit."""
+
+    place = request.form["place"]
+    noun = request.form["noun"]
+    verb = request.form["verb"]
+    adjective = request.form["adjective"]
+    plural_noun = request.form["plural_noun"]
+ 
+    madlib = Story(
+        [place,
+         noun, 
+         verb, 
+         adjective, 
+         plural_noun
+        ],
+        """Once upon a time in a long-ago {place}, there lived a
+        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+    )
+
+    text = madlib.generate(request.form)
+    
+    return render_template("story.html", text=text)
